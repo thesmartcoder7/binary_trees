8d004024b8c98dd4b98aee310c01e6a2c73dbd5e
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-04 03:02:58 +0300

    chore: search for a value in a BST

---
+++ 113-bst_search.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - Searches for a value in a binary search tree.
+ * @tree: A pointer to the root node of the BST to search.
+ * @value: The value to search for in the BST.
+ * Return: a pointer to the node containing the value else NULL.
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree != NULL)
+	{
+		if (tree->n == value)
+			return ((bst_t *)tree);
+		if (tree->n > value)
+			return (bst_search(tree->left, value));
+		return (bst_search(tree->right, value));
+	}
+	return (NULL);
+}
